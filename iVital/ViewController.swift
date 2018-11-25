c640140d3b106d7489748442d0a9d03f6f2dde02
Benson Wu <bensontwu@benson-mbp.local>
2018-11-25 10:19:07 -0800

    Initial Commit

---
+++ iVital/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iVital
+//
+//  Created by Benson Wu on 11/25/18.
+//  Copyright © 2018 Lambda Pledge Class. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
